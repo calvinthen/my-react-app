9a7f7e644c7f1d52f71f0ebdb2c166755ad867ab
Calvin Then <39335528+calvinthen@users.noreply.github.com>
2025-04-17 18:01:23 +0700

    react documentation , last seen displaying data next con render

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import Card from "./Card";
 import Food from "./Food";
 import Footer from "./Footer";
 import Header from "./Header";
+import MyButton from "./MyButton";
+
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
 
 function App() {
 
@@ -10,7 +18,17 @@ function App() {
         {/* <Header/>
         <Food/>
         <Footer/> */}
-        <Card/>
+        {/* <Card/> */}
+        {/* <h1>Welcome to my app</h1>
+        <MyButton/> */}
+
+        <h1>{user.name}</h1>
+        <img className="avatar" src={user.imageUrl} alt={'Photo of ' + user.name} style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }} />
+
+
       </>
     );
 }
---
+++ src/MyButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyButton = () => {
+  return (
+    <div>
+      <button>I'm a button</button>
+    </div>
+  )
+}
+
+export default MyButton
