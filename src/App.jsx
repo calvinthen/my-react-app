e6eb7094faa0d912ccf445550bbef42e7b6e9ba7
Calvin Then <39335528+calvinthen@users.noreply.github.com>
2025-05-07 14:35:07 +0700

    queue a series

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import MyButton from "./MyButton";
 import PackingList from "./PackingList";
 import PassingProps from "./PassingProps";
 import PersonList from "./PersonList";
+import QueueingASeries from "./QueueingASeries";
 import RenderingList from "./RenderingList";
 import RespondingToEvents from "./RespondingToEvents";
 import SculptureList from "./SculptureList";
@@ -112,7 +113,9 @@ function App() {
         {/* <AcomponnentMemory/>
         <AcomponnentMemory/> */}
 
-        <StateAsSnapshot/>
+        {/* <StateAsSnapshot/> */}
+
+        <QueueingASeries/>
       </>
     );
 }
